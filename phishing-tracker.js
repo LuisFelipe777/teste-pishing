577edb653dddaa72b85221b5ca41d53e6b8b364e
Luis Felipe Farias Rodrigues <luis.rodrigues@opiceblum.com.br>
2025-09-30 17:30:59 -0300

    test params

--- phishing-tracker.js
+++ phishing-tracker.js
@@ -5,78 +5,84 @@ const btnSigin = document.querySelector(".btn-sigin");
 let emailDigitado = document.getElementById('emailInput').value;
 let senhaDigitada = document.getElementById('passwordInput').value;
 let dadosRastreio; 
-// 1. Obtém a parte da URL que contém os parâmetros (a partir do '?')
+
+
+// pegar email pelo parâmetro
 const queryString = window.location.search;
-
-// 2. Cria um objeto que facilita a leitura desses parâmetros
 const urlParams = new URLSearchParams(queryString);
+const emailUsuario = urlParams.get('email');
 
-// 3. Usa o método .get() para obter o valor de uma chave específica
-const nomeUsuario = urlParams.get('usuario'); // Retorna 'joao'
-const valorChave = urlParams.get('chave');   // Retorna 'valorSecreto123'
-
-// Exemplo de uso (exibir na página)
-if (nomeUsuario) {
-  console.log(`Parâmetro 'usuario' encontrado: ${nomeUsuario}`);
-  // Você pode injetar isso em algum lugar no HTML:
-  // document.getElementById('bem-vindo').innerText = 'Olá, ' + nomeUsuario;
+if (emailUsuario) {
+   fetch(TRACKING_URL, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(emailUsuario)
+            })
+            .then(response => {
+                console.log('Rastreamento de submissão enviado com sucesso.');
+            })
+            .catch(error => {
+                console.error('Erro ao enviar dados de rastreamento:', error);
+            })
 }
-btnNext.addEventListener('click', () => {
-    console.log("teste");
-    const loginForm = document.getElementById('loginForm');
+
+
+// btnNext.addEventListener('click', () => {
+//     console.log("teste");
+//     const loginForm = document.getElementById('loginForm');
     
-    if (loginForm) {
+//     if (loginForm) {
         
-        loginForm.addEventListener('submit', function(event) {
+//         loginForm.addEventListener('submit', function(event) {
 
-            event.preventDefault(); 
+//             event.preventDefault(); 
             
-            emailDigitado = document.getElementById('emailInput').value;
+//             emailDigitado = document.getElementById('emailInput').value;
 
-              dadosRastreio = {
+//               dadosRastreio = {
 
-                alvo_email: emailDigitado,
+//                 alvo_email: emailDigitado,
                 
-                evento: 'Tentativa_de_Login_Simulado',
-                timestamp: new Date().toISOString()
-            };
+//                 evento: 'Tentativa_de_Login_Simulado',
+//                 timestamp: new Date().toISOString()
+//             };
 
-            fetch(TRACKING_URL, {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(dadosRastreio)
-            })
-            .then(response => {
-                console.log('Rastreamento de submissão enviado com sucesso.');
-            })
-            .catch(error => {
-                console.error('Erro ao enviar dados de rastreamento:', error);
-            })
-            .finally(() => {
-                alert("Sessão expirada. Redirecionando para a página inicial...");
-                window.location.href = TRAINING_URL;
-            });
-        });
-    }
-});
+//             fetch(TRACKING_URL, {
+//                 method: 'POST',
+//                 headers: { 'Content-Type': 'application/json' },
+//                 body: JSON.stringify(dadosRastreio)
+//             })
+//             .then(response => {
+//                 console.log('Rastreamento de submissão enviado com sucesso.');
+//             })
+//             .catch(error => {
+//                 console.error('Erro ao enviar dados de rastreamento:', error);
+//             })
+//             .finally(() => {
+//                 alert("Sessão expirada. Redirecionando para a página inicial...");
+//                 window.location.href = TRAINING_URL;
+//             });
+//         });
+//     }
+// });
 
-btnSigin.addEventListener("click", () => {
+// btnSigin.addEventListener("click", () => {
 
-    console.log("teste");
+//     console.log("teste");
     
-              fetch(TRACKING_URL, {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(dadosRastreio)
-            })
-            .then(response => {
-                console.log('Rastreamento de submissão enviado com sucesso.');
-            })
-            .catch(error => {
-                console.error('Erro ao enviar dados de rastreamento:', error);
-            })
-            .finally(() => {
-                alert("Sessão expirada. Redirecionando para a página inicial...");
-                window.location.href = TRAINING_URL;
-            });
-})
+//               fetch(TRACKING_URL, {
+//                 method: 'POST',
+//                 headers: { 'Content-Type': 'application/json' },
+//                 body: JSON.stringify(dadosRastreio)
+//             })
+//             .then(response => {
+//                 console.log('Rastreamento de submissão enviado com sucesso.');
+//             })
+//             .catch(error => {
+//                 console.error('Erro ao enviar dados de rastreamento:', error);
+//             })
+//             .finally(() => {
+//                 alert("Sessão expirada. Redirecionando para a página inicial...");
+//                 window.location.href = TRAINING_URL;
+//             });
+// })
